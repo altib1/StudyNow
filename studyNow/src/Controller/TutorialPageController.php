588e4ca1ad055c9643fb94a0b99eb915ed7454ad
alti <altiballa@gmail.com>
2022-02-28 02:36:28 +0100

    Feat/add_tutorial fixed

--- studyNow/src/Controller/TutorialPageController.php
+++ studyNow/src/Controller/TutorialPageController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\TutorialRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class TutorialPageController extends AbstractController
 {
     /**
-     * @Route("/tutorial/page", name="tutorial_page")
+     * @Route("/tutorial/{id}", name="tutorial_page", methods={"GET","HEAD"})
      */
-    public function index(): Response
+    public function index($id, TutorialRepository $tutorialRepository,  ManagerRegistry $doctrine): Response
     {
+        $em = $doctrine->getManager();
+        if($tutorialRepository->find($id)) {
+            $tutorial = $tutorialRepository->find($id);
+            /** @var User $user */
+            $user = $this->getUser();
+            $tutorial->addUser($user);
+            $em->persist($tutorial);
+            $em->flush();
+            $this->addFlash('success', 'Congratulations you have added the tutolial!');
+
+        }
         return $this->render('tutorial_page/index.html.twig', [
             'controller_name' => 'TutorialPageController',
+            'tutorial' => $tutorial
+
         ]);
     }
 }
